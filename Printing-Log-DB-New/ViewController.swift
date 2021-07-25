703400f103ff344ff98514ac6bb681695acbac0f
Rich St Onge <richst.ongeywg@gmail.com>
2021-07-25 15:44:02 -0400

    Initial Commit with files Selected

---
+++ Printing-Log-DB-New/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Printing-Log-DB-New
+//
+//  Created by XCodeClub on 2021-07-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
