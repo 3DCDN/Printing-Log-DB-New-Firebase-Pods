70914734d1339579ca5912c9a8145521f92990ae
Rich St Onge <richst.ongeywg@gmail.com>
2021-07-26 14:05:46 -0400

    Start to setup initial interface after completing Firebase setup

---
+++ Printing-Log-DB-New/LogTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LogTableViewCell.swift
+//  Printing-Log-DB-New
+//
+//  Created by XCodeClub on 2021-07-26.
+//
+
+import UIKit
+
+class LogTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
